adf0632653f052aa7e2b323c19990d5eedc6d39a
Matias Santillan <sntlln.93@gmail.com>
2021-08-20 00:45:05 -0300

    fixed bug with notification autoclose 🐛

--- src/contexts/Notifications.js
+++ src/contexts/Notifications.js
@@ -1,4 +1,4 @@
-import { useState, useContext, createContext } from "react";
+import { useState, useContext, createContext, useEffect } from "react";
 import Toasts from "components/Toasts";
 
 const notificationContext = createContext();
@@ -21,12 +21,21 @@ export const useNotification = () => {
 
 const useNotificationProvider = () => {
   const [notifications, setNotifications] = useState([]);
+  const [removing, setRemoving] = useState(null);
+
+  useEffect(() => {
+    setNotifications((n) => n.filter((notif, index) => index !== removing));
+  }, [removing]);
+
+  useEffect(() => {
+    if (notifications.length) {
+      const index = notifications.length - 1;
+      setTimeout(() => setRemoving(index), 3000);
+    }
+  }, [notifications]);
 
   const add = (notification) => {
     setNotifications(notifications.concat(notification));
-    setInterval(() => {
-      remove(notifications.length);
-    }, 4000);
   };
 
   const remove = (indexToBeDeleted) => {
