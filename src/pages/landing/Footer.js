147419499a724ed1f3bec4e5455a35e6f6c83173
Matias Santillan <sntlln.93@gmail.com>
2021-09-03 21:06:44 -0300

    removed social media from footer 🔥

--- src/pages/landing/Footer.js
+++ src/pages/landing/Footer.js
@@ -1,35 +1,7 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Logo from "components/Logo";
-
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__section footer__section--xl">
-        <Logo />
-        <p className="footer__section-copy">
-          Visitanos en nuestras redes sociales
-        </p>
-        <div className="footer__section-social">
-          <div className="footer__section-btn">
-            <a href="https://www.facebook.com/versusbet">
-              <FontAwesomeIcon icon={["fab", "facebook-f"]} />
-            </a>
-          </div>
-          <div className="footer__section-btn">
-            <a href="https://www.facebook.com/versusbet">
-              <FontAwesomeIcon icon={["fab", "twitter"]} />
-            </a>
-          </div>
-          <div className="footer__section-btn">
-            <a href="https://www.facebook.com/versusbet">
-              <FontAwesomeIcon icon={["fab", "instagram"]} />
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div className="footer__section">
-        <h2>JUEGO RESPONSABLE</h2>
+      <div className="footer__disclaimer">
         <small>
           Juegue responsablemente y con moderación. No considere el juego una
           forma de hacer dinero y solo juegue con dinero que pueda permitirse
