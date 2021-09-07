3cc1f7f7a623fc64055d997b12344e59d4e6a0f2
Matias Santillan <sntlln.93@gmail.com>
2021-09-06 21:41:59 -0300

    🛠 This rule is now handle by backend only

--- src/validations/creditSchema.js
+++ src/validations/creditSchema.js
@@ -4,16 +4,13 @@ yup.setLocale({
   mixed: {
     required: "El campo ${path} es obligatorio", // eslint-disable-line
   },
-  number: {
-    min: "El ${path} mínimo a ingresar son ${min} créditos", // eslint-disable-line
-  },
   string: {
     length: "El ${path} debe tener ${length} dígitos", // eslint-disable-line
   },
 });
 
 const creditSchema = yup.object().shape({
-  amount: yup.number().min(50).label("monto"),
+  amount: yup.number().label("monto"),
   phone: yup.string().length(10).label("número de teléfono"),
 });
 
