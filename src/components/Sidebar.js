a383acff9b1a1c344d20ad012f5b433e5944c137
Matias Santillan <sntlln.93@gmail.com>
2022-03-24 19:00:21 -0300

    ♻️ Created sidebarelement

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,13 @@
 import Logo from "components/Logo";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useLocation, useHistory } from "react-router-dom";
-import { faHome, faHistory, faTrophy } from "@fortawesome/free-solid-svg-icons";
+import { SidebarElement } from "./SidebarElement";
+import {
+  faHome,
+  faHistory,
+  faTrophy,
+  faUserFriends,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
-  const { pathname } = useLocation();
-  let history = useHistory();
-
   return (
     <section className="sidebar">
       <div className="sidebar__logo">
@@ -14,39 +15,14 @@ const Sidebar = () => {
       </div>
       <nav>
         <ul className="sidebar__elements">
-          <li
-            className={
-              pathname === "/home"
-                ? "sidebar__element sidebar__element--selected"
-                : "sidebar__element"
-            }
-            onClick={() => history.push("home")}
-          >
-            <FontAwesomeIcon icon={faHome} />
-            <span>Inicio</span>
-          </li>
-          <li
-            className={
-              pathname === "/history"
-                ? "sidebar__element sidebar__element--selected"
-                : "sidebar__element"
-            }
-            onClick={() => history.push("history")}
-          >
-            <FontAwesomeIcon icon={faHistory} />
-            <span>Movimientos</span>
-          </li>
-          <li
-            className={
-              pathname === "/trophies"
-                ? "sidebar__element sidebar__element--selected"
-                : "sidebar__element"
-            }
-            onClick={() => history.push("trophies")}
-          >
-            <FontAwesomeIcon icon={faTrophy} />
-            <span>Logros</span>
-          </li>
+          <SidebarElement icon={faHome} name="Inicio" route="/home" />
+          <SidebarElement
+            icon={faHistory}
+            name="Movimientos"
+            route="/history"
+          />
+          <SidebarElement icon={faTrophy} name="Logros" route="/trophies" />
+          <SidebarElement icon={faUserFriends} name="Amigos" route="/friends" />
         </ul>
       </nav>
     </section>
